4ae67e06f7c9b052df91ffcba58b9b33c604da6e
Bermos <accounts@machivenyika.ch>
2022-11-24 09:02:27 +0100

    Add java client wrapper around the api

--- clients/ev3-java/src/main/java/ch/zhaw/ev3/EV3.java
+++ clients/ev3-java/src/main/java/ch/zhaw/ev3/EV3.java
@@ -1,22 +1,38 @@
 package ch.zhaw.ev3;
 
 
+import ch.zhaw.ev3api.api.MotorApi;
 import ch.zhaw.ev3api.api.PowerApi;
+import ch.zhaw.ev3api.api.SensorApi;
 import ch.zhaw.ev3api.api.SoundApi;
 import ch.zhaw.ev3api.invoker.ApiClient;
 import ch.zhaw.ev3api.invoker.ApiException;
 
 public class EV3 {
     private String host_address;
+    private ApiClient apiClient;
+    private MotorApi motorApi;
+    private PowerApi powerApi;
+    private SensorApi sensorApi;
+    private SoundApi soundApi;
 
     public EV3(String host_address) {
         this.host_address = host_address;
+        this.apiClient = new ApiClient();
+        this.apiClient.setBasePath(String.format("http://%s/api/v1", host_address));
+        System.out.println(this.apiClient.getBasePath());
+
+        this.motorApi = new MotorApi(this.apiClient);
+        this.powerApi = new PowerApi(this.apiClient);
+        this.sensorApi = new SensorApi(this.apiClient);
+        this.soundApi = new SoundApi(this.apiClient);
     }
 
-    public void beep() throws ApiException {
-        ApiClient defaultClient = new ApiClient();
-        defaultClient.setBasePath("http://10.0.100.98:8080/api/v1");
-        SoundApi api = new SoundApi(defaultClient);
-        api.soundBeepPost();
+    public void beep() {
+        try {
+            this.soundApi.soundBeepPost();
+        } catch (ApiException e) {
+            System.out.println(e);
+        }
     }
 }
--- clients/ev3-java/src/test/java/ch/zhaw/ev3/EV3Test.java
+++ clients/ev3-java/src/test/java/ch/zhaw/ev3/EV3Test.java
@@ -1,18 +1,11 @@
 package ch.zhaw.ev3;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class EV3Test {
 
-    @BeforeAll
-    void setup() {
-
-    }
-
     @Test
     void beep() {
+        new EV3("10.0.100.98:8080").beep();
     }
 }
