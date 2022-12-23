38a28f6deefb5bdfbe74f960993db08447e09d79
Bermos <macm.admin@infra-b.ctwoi.ch>
2022-12-23 11:55:24 +0100

    Add more python client test examples

--- clients/ev3-python/test_Buggy.py
+++ clients/ev3-python/test_Buggy.py
@@ -2,7 +2,33 @@ from unittest import TestCase
 
 from ev3.Buggy import Buggy
 
+hostname = "10.0.100.98:8080"
+
 
 class TestBuggy(TestCase):
+    def test_gyro(self):
+        print(Buggy(hostname).gyro())
+
+    def test_distance(self):
+        print(Buggy(hostname).distance())
+
+    def test_stop(self):
+        Buggy(hostname).stop()
+
+    def test_on(self):
+        Buggy(hostname).on(20)
+
+    def test_on_for_degrees(self):
+        Buggy(hostname).on_for_degrees(20, 720)
+
+    def test_on_for_rotations(self):
+        Buggy(hostname).on_for_rotations(20, 2)
+
     def test_on_for_seconds(self):
-        Buggy("10.0.100.98:8080").on_for_seconds(20, 2)
+        Buggy(hostname).on_for_seconds(20, 2)
+
+    def test_steer_counts(self):
+        Buggy(hostname).steer_counts(20, 2, 50)
+
+    def test_steer_duration(self):
+        Buggy(hostname).steer_duration(20, 2, -50)
--- clients/ev3-python/test_EV3.py
+++ clients/ev3-python/test_EV3.py
@@ -2,7 +2,39 @@ from unittest import TestCase
 
 from ev3.EV3 import EV3
 
+hostname = "10.0.100.98:8080"
+
 
 class TestEV3(TestCase):
     def test_beep(self):
-        EV3("10.0.100.98:8080").beep()
+        EV3(hostname).beep()
+
+    def test_play_tone(self):
+        EV3(hostname).play_tone(220, 1)
+
+    def test_speak(self):
+        EV3(hostname).speak("Hello World")
+
+    def test_voltage(self):
+        print(EV3(hostname).voltage())
+
+    def test_current(self):
+        print(EV3(hostname).current())
+
+    def test_max_voltage(self):
+        print(EV3(hostname).max_voltage())
+
+    def test_min_voltage(self):
+        print(EV3(hostname).min_voltage())
+
+    def test_technology(self):
+        print(EV3(hostname).technology())
+
+    def test_button(self):
+        print(EV3(hostname).button())
+
+    def test_flash(self):
+        EV3(hostname).flash()
+
+    def test_led_off(self):
+        EV3(hostname).led_off()
