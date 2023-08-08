45ca884105d6183e018d0354a8c1648a6ddf865f
Patrick Baumgartner <patrick.baumgartner@42talents.com>
2023-08-08 23:13:54 +0200

    Fixing Python

--- clients/ev3-python/setup.py
+++ clients/ev3-python/setup.py
@@ -29,8 +29,10 @@ PYTHON_REQUIRES = ">=3.10"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
     "python-dateutil",
-    "pydantic >= 1.10.5, < 3",
+    "pydantic >= 1.10.5, < 2",
     "aenum",
+    "frozendict",
+    "certifi",
 ]
 
 setup(
